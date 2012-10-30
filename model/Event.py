962a60bddabf0e52c5eb869bc29bf07da07e760e
Martin Dinh <martin@dinhmail.de>
2012-10-30 21:37:01 +0100

    added skeleton for model

---
+++ model/Event.py
@@ -0,0 +1,22 @@
+class Event:
+    def getID():
+        return self.ID
+
+    def getDate():
+        return self.date
+
+    def getComment():
+        return self.comment
+
+    def getAmount():
+        return self.amount
+
+    def getOwner():
+        getOwnerID()
+        return self.owner
+
+    def getOwnerID():
+        return self.ownerID
+
+    def getParticipants():
+        return participants[]
---
+++ model/User.py
@@ -0,0 +1,9 @@
+class User:
+    def getName():
+        return self.name
+
+    def getEmail():
+        return self.email
+
+    def getEvents(status):
+        return events
