5bd6717342efda0e421c2ee23312cfc188728507
Martin Dinh <martin@dinhmail.de>
2012-10-30 21:24:07 +0100

    added DatabaseParser skeleton

---
+++ controller/DatabaseParser.py
@@ -0,0 +1,28 @@
+#
+# Parse database
+#
+
+# login
+    # email
+    # password
+    
+# User exists?
+    # email
+
+# register
+    # email
+    # passwort
+
+# listEvents
+
+# insertEvent
+    # ID[]
+    # date
+    # comment
+    # amount
+    # owner.ID
+    # return Event.ID
+
+# setEventStatus
+    # ID
+    # status
