aa1302eeeb8a322d28d9d12e1fa7242ede441172
Tobias Sturm <tobstu@gmail.com>
2012-10-31 04:01:52 +0100

    improved login urls

--- App.py
+++ App.py
@@ -6,6 +6,5 @@ cherrypy.log.access_file = None
 
 root = splitpot_controller()
 root.user = login_controller()
-root.user.signup = login_controller().register_form
 
 cherrypy.quickstart(root)
--- controller/Login.py
+++ controller/Login.py
@@ -1,7 +1,7 @@
 import cherrypy
 from mako.template import Template
 from mako.lookup import TemplateLookup
-lookup = TemplateLookup(directories=['template/login'])
+lookup = TemplateLookup(directories=['template/login', 'template'])
 import re #gex
 import string
 
@@ -20,19 +20,19 @@ class login_controller(object):
     return tmpl.render()
 
   @cherrypy.expose
-  def register_form(self):
+  def register(self):
     print "provide register form"
     tmpl = lookup.get_template("register.html")
     return tmpl.render()
 
   @cherrypy.expose
-  def forgot_form(self):
+  def forgot(self):
     print "provide forgot form"
     tmpl = lookup.get_template("forgot_pwd.html")
     return tmpl.render()
 
   @cherrypy.expose
-  def login(self, email = None, pwd = None):
+  def doLogin(self, email = None, pwd = None):
     print "login " + email + " with pwd " + pwd #TODO remove!
     tmpl = lookup.get_template("register.html")
     if (not db.login(email, pwd)):
@@ -41,7 +41,7 @@ class login_controller(object):
       return Splitpot.index()
 
   @cherrypy.expose
-  def register(self, email = None, pwd1 = None, pwd2 = None):
+  def doRegister(self, email = None, pwd1 = None, pwd2 = None):
     print "register " + email + ":" + pwd1 + " == " + pwd2 #TODO remove!
     tmpl = lookup.get_template("register.html")
     if(email is None):
@@ -61,7 +61,17 @@ class login_controller(object):
         return tmpl.render(feedback="Something went wrong. Please try again later")
 
   @cherrypy.expose
-  def forgot(self, email = None, resetKey = None):
+  def requestForgot(self, email = None):
+    print "request forgot for " + email
+    tmpl = lookup.get_template("forgot.html")
+    if(not db.userExists(email)):
+      return tmpl.render(feedback="Email not found")
+    else:
+      #TODO send email
+      return tmpl.render(feedback="We sent you further instructions via email")
+
+  @cherrypy.expose
+  def doForgot(self, email = None, resetKey = None):
     print "forgot " + email + ", key " + resetKey
     tmpl = lookup.get_template("forgot.html")
     if(db.isValidResetUrl(email, resetKey)):
