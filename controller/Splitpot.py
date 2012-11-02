7fe4b204e85c3e8fe2c93d238bd6dad9a8ace0b5
Tobias Sturm <tobstu@gmail.com>
2012-11-02 11:44:01 +0100

    my dirty working tree 4 merge

--- controller/Splitpot.py
+++ controller/Splitpot.py
@@ -9,12 +9,17 @@ class splitpot_controller(object):
   @cherrypy.expose
   def index(self):
     tmpl = lookup.get_template("index.html")
-    return tmpl.render()
+    return tmpl.render(debts=12.2, others_debts=0.2, entries = [])
 
-  def add(self):
+  def add(self): #gives the form for entering a new event
     tmpl = lookup.get_template("add.html")
     return tmpl.render()
 
-#  def doAdd(self, Comment, Amount, Others):
- #TODO continue here
+  """
+  Adds an event with the current user as owner and users with emails in 'others' as participants
+
+  If one of the given emails in other is not a known user, an invitation email will be sent.
+  """
+  def doAdd(self, comment, amount, others):
+    print "do Add"
 
