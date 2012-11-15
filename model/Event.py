129f1f4caaee7d1096f70e6758d6825a8bea6144
Tobias Sturm <tobstu@gmail.com>
2012-11-15 12:39:02 +0100

    add toString

--- model/Event.py
+++ model/Event.py
@@ -1,3 +1,5 @@
+import json
+
 class Event:
     def __init__(self, id=None, owner=None, date=None, amount=None, participants=None, comment=None):
         self.id = id
@@ -6,3 +8,6 @@ class Event:
         self.amount = amount
         self.participants = participants
         self.comment = comment
+
+    def __str__(self):
+     return "Event " + str(self.id) + ", owned by " + self.owner + "(" + self.date + ") over " + str(self.amount) + " with " + json.dumps(self.participants) + " | " + self.comment
