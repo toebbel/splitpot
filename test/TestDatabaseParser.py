b596011d1060d89bef3ecafa266d8722de2efaaa
Martin Dinh <martin@dinhmail.de>
2012-11-22 13:55:01 +0100

    added test cases for merging users

--- test/TestDatabaseParser.py
+++ test/TestDatabaseParser.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import unittest
 import sys
 from datetime import datetime
@@ -6,71 +9,137 @@ sys.path.append('controller/')
 
 from DatabaseParser import *
 
+
 class TestDatabaseParser(unittest.TestCase):
 
-  def setUp(self):
-    """
+    def setUp(self):
+        """
     Called before *each* testcase. We want to operate on a fresh database
     """
-    clear()
 
-  def testVerifyLoginOnEmptyDb(self):
-    self.assertFalse(verifyLogin("test@0xabc.de", "asfelkj"))
+        clear()
 
-  def testRegister(self):
-    self.assertTrue(registerUser("benjamin@flowerpower.org", "Beni", "schokolade"))
-    self.assertFalse(registerUser("benJamin@flowerpower.org", "Benjamin", "schokolade"))
-    self.assertTrue(registerUser("karlaColumna@bild.de", "Carla", "pwd"))
-    self.assertTrue(userExists("benjamin@flowerpower.org"))
-    self.assertTrue(userExists("karlaColumna@bild.de"))
+    def testVerifyLoginOnEmptyDb(self):
+        self.assertFalse(verifyLogin('test@0xabc.de', 'asfelkj'))
 
-  def testListEvents(self):
-    registerUser("a", "alpha", "1")
-    registerUser("b", "beta", "2")
-    registerUser("c", "charlie", "3")
-    date_a = datetime.now
-    id_a = insertEvent("a", "1.4.2013", 10, ["b"], "Event1")
-    print listEvents()
-    self.assertEqual(listEvents(), [(id_a, u'a', '1.4.2013', 10, u'["b"]', u'Event1')])
+    def testRegister(self):
+        self.assertTrue(registerUser('benjamin@flowerpower.org', 'Beni'
+                        , 'schokolade'))
+        self.assertFalse(registerUser('benJamin@flowerpower.org',
+                         'Benjamin', 'schokolade'))
+        self.assertTrue(registerUser('karlaColumna@bild.de', 'Carla',
+                        'pwd'))
+        self.assertTrue(userExists('benjamin@flowerpower.org'))
+        self.assertTrue(userExists('karlaColumna@bild.de'))
 
-  def testListEvents_emptyDB(self):
-    listEvents()
+    def testListEvents(self):
+        registerUser('a', 'alpha', '1')
+        registerUser('b', 'beta', '2')
+        registerUser('c', 'charlie', '3')
+        date_a = datetime.now
+        id_a = insertEvent('a', '1.4.2013', 10, ['b'], 'Event1')
+        print listEvents()
+        self.assertEqual(listEvents(), [(
+            id_a,
+            u'a',
+            '1.4.2013',
+            10,
+            u'["b"]',
+            u'Event1',
+            )])
 
-  def testGetPassword_NonexistentUser(self):
-    getPassword("martin@0xabc.de")
+    def testListEvents_emptyDB(self):
+        listEvents()
 
-  def testGetPassword(self):
-    registerUser("awesome@0xabc.de", "Bluemchen", "blume")
-    getPassword("awesome@0xabc.de")
+    def testGetPassword_NonexistentUser(self):
+        getPassword('martin@0xabc.de')
 
-  def testInsertEvent(self):
-    registerUser("awesome@0xabc.de", "Bluemchen", "blume")
-    insertEvent("awesome@0xabc.de", "4.1.2010", 12.1, ["tobstu@0xabc.de"], "An Event")
+    def testGetPassword(self):
+        registerUser('awesome@0xabc.de', 'Bluemchen', 'blume')
+        getPassword('awesome@0xabc.de')
 
-  def testSetEventStatus(self):
-    setEventStatus("tobstu@gmail.com", 2, "paid")
+    def testInsertEvent(self):
+        registerUser('awesome@0xabc.de', 'Bluemchen', 'blume')
+        insertEvent('awesome@0xabc.de', '4.1.2010', 12.1,
+                    ['tobstu@0xabc.de'], 'An Event')
 
-  def testResetUrl(self):
-    registerUser("dummy@0xabc.de", "dummy", "thisIsMyPwd")
-    reset = getResetUrlKey("dummy@0xabc.de")
-    self.assertTrue(str(reset).__len__() == 8)
-    self.assertTrue(isValidResetUrlKey("dummy@0xabc.de", reset))
+    def testSetEventStatus(self):
+        setEventStatus('tobstu@gmail.com', 2, 'paid')
 
-  def testResetLogin(self):
-    registerUser("userA@0xabc.de", "A", "123456")
-    registerUser("userB@0xabc.de", "B", "654321")
-    self.assertFalse(updateLogin("userC@0xabc.de", "ffffff"))
-    self.assertTrue(updateLogin("userb@0xabc.de", "abcdef"))
-    self.assertFalse(verifyLogin("userB@0xabc.de", "654321"))
-    self.assertTrue(verifyLogin("userB@0xabc.de", "abcdef"))
-    self.assertTrue(verifyLogin("userA@0xabc.de", "123456"))
+    def testResetUrl(self):
+        registerUser('dummy@0xabc.de', 'dummy', 'thisIsMyPwd')
+        reset = getResetUrlKey('dummy@0xabc.de')
+        self.assertTrue(str(reset).__len__() == 8)
+        self.assertTrue(isValidResetUrlKey('dummy@0xabc.de', reset))
 
-  def testGetEvent(self):
-    registerUser("userA@0xabc.de", "A", "123456")
-    id = insertEvent("userA@0xabc.de", "10.4.2013", 101.12, ["tobstu@gmail.com"], "comment")
-    self.assertEqual(getEvent(id + 1), None)
-    print str(getEvent(id))
-    self.assertEqual(str(getEvent(id)), str(Event(id=id, owner="userA@0xabc.de", date="10.4.2013", amount=101.12, participants=['tobstu@gmail.com'], comment="comment")))
+    def testResetLogin(self):
+        registerUser('userA@0xabc.de', 'A', '123456')
+        registerUser('userB@0xabc.de', 'B', '654321')
+        self.assertFalse(updateLogin('userC@0xabc.de', 'ffffff'))
+        self.assertTrue(updateLogin('userb@0xabc.de', 'abcdef'))
+        self.assertFalse(verifyLogin('userB@0xabc.de', '654321'))
+        self.assertTrue(verifyLogin('userB@0xabc.de', 'abcdef'))
+        self.assertTrue(verifyLogin('userA@0xabc.de', '123456'))
+
+    def testGetEvent(self):
+        registerUser('userA@0xabc.de', 'A', '123456')
+        id = insertEvent('userA@0xabc.de', '10.4.2013', 101.12,
+                         ['tobstu@gmail.com'], 'comment')
+        self.assertEqual(getEvent(id + 1), None)
+        print str(getEvent(id))
+        self.assertEqual(str(getEvent(id)), str(Event(
+            id=id,
+            owner='userA@0xabc.de',
+            date='10.4.2013',
+            amount=101.12,
+            participants=['tobstu@gmail.com'],
+            comment='comment',
+            )))
+
+    def testMergeUser(self):
+        registerUser('jobs@0xabc.de', 'Steve Jobs', 'apple')
+        registerUser('gates@0xabc.de', 'Bill Gates', 'microsoft')
+        registerUser('buffet@0xabc.de', 'Warren Buffet', 'billion')
+        id = insertEvent('jobs@0xabc.de', '2012-02-20', 21.22,
+                         ['gates@0xabc.de', 'buffet@0xabc.de'],
+                         'Dinner with my besties')
+        insertEvent('gates@0xabc.de', '2012-12-20', 1.22,
+                    ['buffet@0xabc.de'], 'Dinner with my besties')
+
+        self.assertFalse(mergeUser('sinofsky@0xabc.de', 'jobs@0xabc.de'
+                         ))
+
+                             # sinofski doesn't exist
+
+        self.assertTrue(mergeUser('gates@0xabc.de', 'jobs@0xabc.de'))
+
+        self.assertNotEqual(str(getEvent(id)), str(Event(
+            id=id,
+            owner='jobs@0xabc.de',
+            date='2012-02-20',
+            amount=21.22,
+            participants=['gates@0xabc.de', 'buffet@0xabc.de'],
+            comment='Dinner with my besties',
+            )))
+
+        self.assertEqual(str(getEvent(id)), str(Event(
+            id=id,
+            owner='gates@0xabc.de',
+            date='2012-02-20',
+            amount=21.22,
+            participants=['gates@0xabc.de', 'buffet@0xabc.de'],
+            comment='Dinner with my besties',
+            )))
+
+        self.assertEqual(str(getEvent(id + 1)), str(Event(
+            id=id + 1,
+            owner='gates@0xabc.de',
+            date='2012-12-20',
+            amount=1.22,
+            participants=['buffet@0xabc.de'],
+            comment='Dinner with my besties',
+            )))
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
