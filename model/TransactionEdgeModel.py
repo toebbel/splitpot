d454d661ec48a100ccd4201fdfc9da67ee8c936e
Tobias Sturm <tobstu@gmail.com>
2013-01-25 01:38:56 +0100

    last payday fix.closes #17

--- model/TransactionEdgeModel.py
+++ model/TransactionEdgeModel.py
@@ -1,19 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
 class TransactionEdge:
-    def __init__(self, fromUserId, toUserId, amount):
+
+    def __init__(
+        self,
+        fromUserId,
+        toUserId,
+        amount,
+        ):
+
         self.fromUser = fromUserId
         self.toUser = toUserId
         self.amount = amount
 
     def keyify(self):
-        return str(self.fromUser) + "|" + str(self.toUser)
+        return str(self.fromUser) + '|' + str(self.toUser)
 
     def __eq__(self, other):
         if isinstance(other, TransactionEdge):
-            return self.fromUser == other.fromUser and self.toUser == other.toUser and (self.amount - other.amount < 0.01)
-        return TypeError("can't compare Transaction edge to " + str(other))
+            return self.fromUser == other.fromUser and self.toUser \
+                == other.toUser and self.amount - other.amount < 0.01
+        return TypeError("can't compare Transaction edge to "
+                         + str(other))
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
     def __str__(self):
-        return "from " + self.fromUser + ", " + self.toUser + ", amount " + str(self.amount)
+        return 'from ' + self.fromUser + ', ' + self.toUser \
+            + ', amount ' + str(self.amount)
+
+
