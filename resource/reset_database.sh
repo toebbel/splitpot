04463df1bd92ee50212eb9b226ddb6c9bd155dcd
Tobias Sturm <tobstu@gmail.com>
2013-01-25 05:07:08 +0100

    backup script & gitignore update

--- resource/reset_database.sh
+++
@@ -1,5 +0,0 @@
-if [ -f splitpotDB_DEV.sqlite ]
-then
-    rm splitpotDB_DEV.sqlite
-fi
-echo ".read sample_db.dump" | sqlite3 splitpotDB_DEV.sqlite
---
+++ script/backup_database.sh
@@ -0,0 +1,7 @@
+if [ -f resource/splitpotDB.sqlite ]
+then
+    DATE=$(date +"%y-%m-%d-%h-%m-%s")
+    BACKUP_FILE="backup/splitpotDB.sqlite.$DATE.backup"
+    echo ".dump" | sqlite3 -batch -echo resource/splitpotDB_DEV.sqlite > "$BACKUP_FILE"
+    echo "writing backup to $BACKUP_FILE"
+fi
