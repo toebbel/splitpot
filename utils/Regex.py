272254854c072398d887c35d8da02c33aae820bd
Tobias Sturm <tobstu@gmail.com>
2013-02-13 17:51:15 +0100

    close #44

--- utils/Regex.py
+++ utils/Regex.py
@@ -4,7 +4,7 @@
 import re  # gex
 
 entryCommentRegex = \
-    re.compile("\A([\w|\d\|\s|\.|-|\20AC|,|:|/|\(|\)])*\Z",
+    re.compile("\A([\w|\d\|\s|\+|\.|-|\20AC|,|:|/|\(|\)])*\Z",
                re.MULTILINE | re.UNICODE)
 emailRegex = \
     re.compile(r"(?:^|\s)[-a-z0-9_.+]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}(?:\s|$)"
