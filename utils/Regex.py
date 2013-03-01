8e0dc189c67ceca09d7178ab73a335a9ad2b0d93
Martin Dinh <martin@dinhmail.de>
2013-03-01 16:26:29 +0100

    \20AC couldn't be replaced by €

--- utils/Regex.py
+++ utils/Regex.py
@@ -4,8 +4,8 @@
 import re  # gex
 
 entryCommentRegex = \
-    re.compile("\A([\w|ü|Ü|ä|Ä|ö|Ö|ß|\d\|\s|\.|-|€|,|:|/|\(|\)|\-|\+|\*])*\Z",
-               re.MULTILINE | re.UNICODE)
+    re.compile("\A([\w|ü|Ü|ä|Ä|ö|Ö|ß|\d\|\s|\.|-|\20AC|,|:|!|/|\(|\)|\-|\+|\*])*\Z"
+               , re.MULTILINE | re.UNICODE)
 emailRegex = \
     re.compile(r"(?:^|\s)[-a-z0-9_.+]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}(?:\s|$)"
                , re.IGNORECASE)
