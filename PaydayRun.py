87996aa49e254083b7f16d7c12d53869cc427ab3
Martin Dinh <martin@dinhmail.de>
2013-05-17 10:35:19 +0200

    remove backup in payday routine, because we have a backup every day anyway

--- PaydayRun.py
+++ PaydayRun.py
@@ -7,13 +7,6 @@ import os
 import datetime
 now = datetime.datetime.now()
 
-# backup the database
-
-bashCmd = \
-    'echo ".dump" | sqlite3 -batch -echo resource/splitpotDB_DEV.sqlite > ' \
-    + now.strftime('%Y-%m-%d-%H-%M-%S') + '.backup'
-os.system(bashCmd)
-
 # run payday
 
 from controller.Splitpot import *
